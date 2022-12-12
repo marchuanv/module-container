029666ad9dbff0b9495b0309c391c363482097dd
marchuanv <Marchuanv@gmail.com>
2022-12-13 01:10:01 +0200

    Update index.js

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const utils = require("utils");
 const server = http.createServer();
 const { Octokit, App } = require("octokit");
 const octokit = new Octokit({ auth: process.env.GIT });
-octokit.request("GET /repos/{OWNER}/{REPO}/brances").then((info) => {
+octokit.request("GET /repos/owner/repo/brances").then((info) => {
  console.log(info);
  server.on("request", (request, response) => {
   console.log("request received");
