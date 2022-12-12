33d51778e643a00811f90df3fd2dd62b8b3bafe3
marchuanv <Marchuanv@gmail.com>
2022-12-12 03:05:07 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ const server = http.createServer();
 server.on("request", (request, response) => {
   console.log("request received");
   const results = {
-     statusCode: 404
+     statusCode: 404,
      statusMessage: 'Not Found',
      message: 'Not Found'
   }:
   if (request.path === '/active') {
      results.statusCode = 200;
-     results.statusMessage = "Success";
+     results.statusMessage = 'Success';
      results.message 'no active objects';
   }
   const bodyStr = utils.getJSONString(results);
