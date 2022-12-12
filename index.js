1df94dc15f9ba45320d2d3f283356d1aec1c1059
marchuanv <Marchuanv@gmail.com>
2022-12-12 02:27:51 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const server = http.createServer();
-server.on("request", (request) => {
+server.on("request", (request, response) => {
   console.log("request received");
+  response.write("hello world");
 });
 server.listen(process.env.PORT || 8080);
