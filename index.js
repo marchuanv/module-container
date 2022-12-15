1ff6c422db3ccfa41fb5b3872770e6b520405066
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2022-12-16 01:55:44 +0200

    making progress

--- index.js
+++ index.js
@@ -5,83 +5,117 @@ const server = http.createServer();
 const { Octokit } = require("octokit");
 const octokit = new Octokit({ auth: process.env.GIT });
 
-server.on("request", async (request, response) => {
-    console.log("request received");
-    const results = {
-        statusCode: 404,
-        statusMessage: 'Not Found',
-        message: 'No Active Objects Found'
-    };
-    const urlSplit = request.url.split('/').filter(x => x);
-    console.log('url segments: ', utils.getJSONString(urlSplit));
-    const aoName = urlSplit[0];
-
-    console.log(`context: ${aoName}`);
-
-    let branchExists = false;
-    try {
-        await octokit.request(`GET /repos/marchuanv/active-objects/branches/${aoName}`);
-        branchExists = true;
-    } catch (error) {
-        if (error.status === 404) {
-            branchExists = false;
-        } else {
-            throw (error);
-        }
-    }
-
-    let revision = "";
-    try {
-        const { data } = await octokit.request(`GET /repos/marchuanv/active-objects/git/refs/heads`);
-        revision = data.shift().object.sha;
-        console.log('latest revision: ', revision);
-    } catch (error) {
-        throw (error);
-    }
-    if (request.method === 'GET') {
-        const { data } = await octokit.request(`GET /repos/marchuanv/active-objects/contents/${aoName}.js`);
-        https.get(data.download_url, (resp) => {
-            let data = '';
-            resp.on('data', (chunk) => {
-                data += chunk;
-            });
-            resp.on('end', () => {
-                console.log(JSON.parse(data).explanation);
-            });
-        }).on("error", (err) => {
-            console.log("Error: " + err.message);
-        });
-    } else if (aoName && request.method === 'PUT' && !branchExists) {
-        console.log(`${aoName} does not exist.`);
-        try {
-            await octokit.request(`POST /repos/marchuanv/active-objects/git/refs`, {
-                ref: `refs/heads/${aoName}`,
-                sha: revision
-            });
-            results.statusCode = 200;
-            results.statusMessage = 'Success';
-            results.message = 'Active Object Created';
-            results.name = aoName;
-        } catch (error) {
-            throw (error);
-        }
-    } else if (aoName && request.method === 'DELETE' && branchExists) {
-        try {
-            await octokit.request(`DELETE /repos/marchuanv/active-objects/git/refs/heads/${aoName}`);
-            results.statusCode = 200;
-            results.statusMessage = 'Success';
-            results.message = 'Active Object Deleted';
-            results.name = aoName;
-        } catch (error) {
-            throw (error);
-        }
-    }
-
-    const bodyStr = utils.getJSONString(results);
-    response.writeHead(results.statusCode, results.statusMessage, {
-        'Content-Length': Buffer.byteLength(bodyStr),
-        'Content-Type': 'application/json'
+server.on("request", (request, response) => {
+    let content = '';
+    request.on('data', (chunk) => {
+        content += chunk;
     });
-    response.end(bodyStr);
+    request.on('end', async () => {
+        console.log("request received");
+        const results = {
+            statusCode: 404,
+            statusMessage: 'Not Found',
+            message: 'No Active Objects Found'
+        };
+        const urlSplit = request.url.split('/').filter(x => x);
+        console.log('url segments: ', utils.getJSONString(urlSplit));
+        const aoName = urlSplit[0];
+    
+        if (aoName !== 'main') {
+            let branchExists = false;
+            try {
+                await octokit.request(`GET /repos/marchuanv/active-objects/branches/${aoName}`);
+                branchExists = true;
+            } catch (error) {
+                console.error(error);
+            }
+            let revision = "";
+            try {
+                const { data } = await octokit.request(`GET /repos/marchuanv/active-objects/git/refs/heads`);
+                revision = data.shift().object.sha;
+                console.log('latest revision: ', revision);
+            } catch (error) {
+                console.error(error);
+            }
+            if (request.method === 'GET') {
+                try {
+                    const { data } = await octokit.request(`GET /repos/marchuanv/active-objects/contents/${aoName}.js?ref=${aoName}`);
+                    https.get(data.download_url, (resp) => {
+                        content = "";
+                        resp.on('data', (chunk) => content += chunk);
+                    }).on("error", (err) => {
+                        console.log("Error: " + err.message);
+                    });
+                    results.statusCode = 200;
+                    results.statusMessage = 'Success';
+                    results.message = 'Active Object Info';
+                    results.name = aoName;
+                    results.data = content;
+                } catch(error) {
+                    console.error(error);
+                }
+            } else if (aoName && request.method === 'PUT') {
+                if (!branchExists) {
+                    try {
+                        await octokit.request(`POST /repos/marchuanv/active-objects/git/refs`, {
+                            ref: `refs/heads/${aoName}`,
+                            sha: revision
+                        });
+                    } catch(error) {
+                        console.error(error);
+                    }
+                }
+                let fileSha = undefined;
+                try {
+                    const { data } = await octokit.request(`GET /repos/marchuanv/active-objects/contents/${aoName}.js?ref=${aoName}`);
+                    fileSha = data?.sha;
+                } catch(error) {
+                    console.error(error);
+                }
+                let message = `${aoName}.js script updated`;
+                if (!fileSha) {
+                    message = `${aoName}.js script created`;
+                }
+                try {
+                    await octokit.request(`${request.method} /repos/marchuanv/active-objects/contents/${aoName}.js`, {
+                        owner: 'marchuanv',
+                        repo: 'active-objects',
+                        path: `/${aoName}.js`,
+                        message,
+                        branch: aoName,
+                        committer: {
+                        name: 'active-objects-admin',
+                        email: 'active-objects-admin@gmail.com'
+                        },
+                        content: utils.stringToBase64(content),
+                        sha: fileSha
+                    });
+                } catch(error) {
+                    console.error(error);
+                }
+                results.statusCode = 200;
+                results.statusMessage = 'Success';
+                results.message = 'Active Object Created/Updated';
+                results.name = aoName;
+            } else if (aoName && request.method === 'DELETE' && branchExists) {
+                try {
+                    await octokit.request(`DELETE /repos/marchuanv/active-objects/git/refs/heads/${aoName}`);
+                    results.statusCode = 200;
+                    results.statusMessage = 'Success';
+                    results.message = 'Active Object Deleted';
+                    results.name = aoName;
+                } catch (error) {
+                    console.error(error);
+                }
+            }
+        }
+       
+        const bodyStr = utils.getJSONString(results);
+        response.writeHead(results.statusCode, results.statusMessage, {
+            'Content-Length': Buffer.byteLength(bodyStr),
+            'Content-Type': 'application/json'
+        });
+        response.end(bodyStr);
+    });
 });
 server.listen(process.env.PORT || 8080);
