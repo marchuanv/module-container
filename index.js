7ad03159c4eee23ec6bcbdf9356b6744bc4b09a2
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2022-12-16 02:18:34 +0200

    making progress

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const http = require("http");
 const fetch = require("./fetch");
 const utils = require("utils");
+const vm = require('node:vm');
 const server = http.createServer();
 const { Octokit } = require("octokit");
 const octokit = new Octokit({ auth: process.env.GIT });
@@ -22,6 +23,7 @@ server.on("request", (request, response) => {
         const aoName = urlSplit[0];
     
         if (aoName !== 'main') {
+
             let branchExists = false;
             try {
                 await octokit.request(`GET /repos/marchuanv/active-objects/branches/${aoName}`);
@@ -71,25 +73,44 @@ server.on("request", (request, response) => {
                     message = `${aoName}.js script updated`;
                 }
                 try {
-                    await octokit.request(`${request.method} /repos/marchuanv/active-objects/contents/${aoName}.js`, {
-                        owner: 'marchuanv',
-                        repo: 'active-objects',
-                        path: `/${aoName}.js`,
-                        message,
-                        branch: aoName,
-                        committer: {
-                        name: 'active-objects-admin',
-                        email: 'active-objects-admin@gmail.com'
-                        },
-                        content: utils.stringToBase64(content),
-                        sha: fileSha
-                    });
+                    if (content) {
+                        let isValidScript = true;
+                        try {
+                            const context = {};
+                            vm.createContext(context); // Contextify the object.
+                            vm.runInContext(content, context);
+                        } catch (error) {
+                            console.error(error);
+                            isValidScript = false;
+                            results.statusCode = 422;
+                            results.statusMessage = 'Unprocessable Entity';
+                            results.message = 'Active Object Script Error';
+                            results.error = error.message;
+                            results.stack = error.stack;
+                        }
+                        if (isValidScript) {
+                            await octokit.request(`${request.method} /repos/marchuanv/active-objects/contents/${aoName}.js`, {
+                                owner: 'marchuanv',
+                                repo: 'active-objects',
+                                path: `/${aoName}.js`,
+                                message,
+                                branch: aoName,
+                                committer: {
+                                name: 'active-objects-admin',
+                                email: 'active-objects-admin@gmail.com'
+                                },
+                                content: utils.stringToBase64(content),
+                                sha: fileSha
+                            });
+                            results.statusCode = 200;
+                            results.statusMessage = 'Success';
+                            results.message = 'Active Object Created/Updated';
+                        }
+                    }
                 } catch(error) {
                     console.error(error);
                 }
-                results.statusCode = 200;
-                results.statusMessage = 'Success';
-                results.message = 'Active Object Created/Updated';
+              
             } else if (aoName && request.method === 'DELETE' && branchExists) {
                 try {
                     await octokit.request(`DELETE /repos/marchuanv/active-objects/git/refs/heads/${aoName}`);
