6e23e2ef7e1a367075d440c2eba0ca4576ece6dd
marchuanv <Marchuanv@gmail.com>
2022-12-17 22:57:57 +0200

    Update and rename github-get-file.cs to github-file.cs

---
+++ github-file.cs
@@ -0,0 +1,24 @@
+const github = require('./github');
+module.exports = {
+   getContent: async ({ branchName, fileName }) => {
+      const octokit = github.login();
+      const { data } = await octokit.request(`GET /repos/marchuanv/active-objects/contents/${branchName}.js?ref=${fileName}`);
+      return await fetch({ url: data.download_url});
+   },
+   createContent: async ({ branchName, fileName }) => {
+      const octokit = github.login();
+      await octokit.request(`${request.method} /repos/marchuanv/active-objects/contents/${fileName}.js`, {
+         owner: 'marchuanv',
+         repo: 'active-objects',
+         path: `/${fileName}.js`,
+         message: 'created/updated', 
+         branch: branchName,
+         committer: {
+             name: 'active-objects-admin',
+             email: 'active-objects-admin@gmail.com'
+         }, 
+         content: utils.stringToBase64(content),
+         sha: fileSha
+      });
+   }
+}
