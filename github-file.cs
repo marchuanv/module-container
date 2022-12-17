373173d38303c6e6eb27456c3fa25ed1bc5efc7f
marchuanv <Marchuanv@gmail.com>
2022-12-17 22:59:00 +0200

    Update github-file.cs

--- github-file.cs
+++ github-file.cs
@@ -1,12 +1,12 @@
 const github = require('./github');
 module.exports = {
    getContent: async ({ branchName, fileName }) => {
-      const octokit = github.login();
+      const octokit = await github.login();
       const { data } = await octokit.request(`GET /repos/marchuanv/active-objects/contents/${branchName}.js?ref=${fileName}`);
       return await fetch({ url: data.download_url});
    },
    createContent: async ({ branchName, fileName }) => {
-      const octokit = github.login();
+      const octokit = await github.login();
       await octokit.request(`${request.method} /repos/marchuanv/active-objects/contents/${fileName}.js`, {
          owner: 'marchuanv',
          repo: 'active-objects',
