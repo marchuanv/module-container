d688927d4173fb30d7c605da290e1b40dd74b3ce
marchuanv <Marchuanv@gmail.com>
2022-12-17 22:41:14 +0200

    Update github-get-file.cs

--- github-get-file.cs
+++ github-get-file.cs
@@ -1,2 +1,12 @@
 const github = require('./github');
-const octokit = github.login();
+module.exports = {
+   getContent: ({ branchName, fileName }) => {
+      try {
+         const octokit = github.login();
+         const { data } = await octokit.request(`GET /repos/marchuanv/active-objects/contents/${branchName}.js?ref=${fileName}`);
+         return await fetch({ url: data.download_url});
+      } catch(error) {
+         console.error(error);
+      }
+   }
+}
