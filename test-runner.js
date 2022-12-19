5ad23066c46c981c6e0e5a82914d1a659a763853
marchuanv <Marchuanv@gmail.com>
2022-12-19 16:45:15 +0200

    Update test-runner.js

--- test-runner.js
+++ test-runner.js
@@ -1,6 +1,17 @@
+require('./logging')('info');
 module.exports = {
   runTest: async ({ moduleName, functionName, testParams }) => {
     const module = require(`./${moduleName}`)(testParams);
     await module[functionName](testParams);
+    return {
+      assert: async (callback) => {
+        const results = await callback();
+        if(results) {
+          console.log('TEST PASSED');
+        } else {
+          console.log('TEST FAILED');
+        }
+      }
+    };
   }
 }
