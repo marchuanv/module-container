ba2911ad9eff29a616516ed5934e3fcbd00ed046
marchuanv <Marchuanv@gmail.com>
2022-12-19 16:51:44 +0200

    Update test-runner.js

--- test-runner.js
+++ test-runner.js
@@ -2,11 +2,11 @@ require('./logging')('info');
 module.exports = {
   runTest: async ({ moduleName, functionName, testParams }) => {
     const module = require(`./${moduleName}`)(testParams);
-    await module[functionName](testParams);
+    const results = await module[functionName](testParams);
     return {
       assert: async (callback) => {
-        const results = await callback();
-        if(results) {
+        const res = await callback(results);
+        if(res) {
           console.log('TEST PASSED');
         } else {
           console.log('TEST FAILED');
