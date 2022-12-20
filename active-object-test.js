41c5b5a0c901086dd61ed1c9a06e90469f704fb7
marchuanv <Marchuanv@gmail.com>
2022-12-20 16:57:11 +0200

    Update active-object-test.js

--- active-object-test.js
+++ active-object-test.js
@@ -1,15 +1,15 @@
 const { runTest } = require('./test-runner.js');
 const moduleName = 'active-object';
-const branchName = 'test';
 const privateKey = process.env.GIT;
 const url = '/doTest1/doTest2/doTest3';
 const objectScript = `function test() {
-  this.doTest1 = () => {console.log('test 01')};
-  this.doTest2 = () => {console.log('test 02')};
-  this.doTest3 = () => {console.log('test 03')};
+  this.doTest1 = () => console.log('test 01');
+  this.doTest2 = () => console.log('test 02');
+  this.doTest3 = () => console.log('test 03');
 }`;
+const input = {};
 
 ( async () => {
-   (await runTest({ moduleName, functionName: 'validate', testParams: { url, objectScript } })).assert(() => true);
-   (await runTest({ moduleName, functionName: 'call', testParams: { url, objectScript } })).assert((res) => res);
+   (await runTest({ moduleName, functionName: 'validate', testParams: { url, objectScript, input } })).assert(() => true);
+   (await runTest({ moduleName, functionName: 'call', testParams: { url, objectScript, input } })).assert((res) => res);
 })();
