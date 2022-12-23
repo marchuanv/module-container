016beafab85a54186c05da5e8b035f91c325b590
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2022-12-23 10:15:37 +0200

    moving tests around 2

--- tests/github-test.js
+++ tests/github-test.js
@@ -2,15 +2,8 @@ const { test } = require('./test-runner.js');
 const privateKey = process.env.GIT;
 
 ( async () => {
-   let moduleName = 'github-file';
-   let branchName = 'test';
-
-   const fileName = 'test';
-   const content = 'github-file-content-test';
-   await test({ moduleName, functionName: 'deleteFile', testParams: { privateKey, branchName, fileName } }).assert((res) => !res);
-   await test({ moduleName, functionName: 'ensureFileContent', testParams: { privateKey, branchName, fileName, content } }).assert(() => true);
-   await test({ moduleName, functionName: 'getFileMetadata', testParams: { privateKey, branchName, fileName } }).assert((res) => res?.sha);
-   await test({ moduleName, functionName: 'getFileContent', testParams: { privateKey, branchName, fileName } }).assert((res) => res === content);
+   let moduleName = '';
+   let branchName = '';
 
    moduleName = 'github-branch';
    branchName = 'test';
@@ -18,5 +11,14 @@ const privateKey = process.env.GIT;
    await test({ moduleName, functionName: 'isExisting', testParams: { privateKey, branchName } }).assert((res) => !res);
    await test({ moduleName, functionName: 'create', testParams: { privateKey, branchName } }).assert(() => true);
    await test({ moduleName, functionName: 'isExisting', testParams: { privateKey, branchName } }).assert((res) => res);
+
+   moduleName = 'github-file';
+   branchName = 'test';
+   const fileName = 'test';
+   const content = 'github-file-content-test';
+   await test({ moduleName, functionName: 'deleteFile', testParams: { privateKey, branchName, fileName } }).assert((res) => !res);
+   await test({ moduleName, functionName: 'ensureFileContent', testParams: { privateKey, branchName, fileName, content } }).assert(() => true);
+   await test({ moduleName, functionName: 'getFileMetadata', testParams: { privateKey, branchName, fileName } }).assert((res) => res?.sha);
+   await test({ moduleName, functionName: 'getFileContent', testParams: { privateKey, branchName, fileName } }).assert((res) => res === content);
 
 })();
