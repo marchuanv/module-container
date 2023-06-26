0db9a643594774687cea558364d468e44d56f8b0
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2023-06-26 23:55:34 +0200

    starting work to save session to disk

--- tests/active-object-test-fail.js
+++ tests/active-object-test-fail.js
@@ -12,7 +12,7 @@ const sessionId = 'c62120de-30d4-40a1-8fef-0a6133efdee6';
   const { createSession } = await require('../lib/store');
   const { session } = await createSession({ sessionId });
   const { writeFileSync } = session;
-  writeFileSync(scriptFilePath, script);
+  await writeFileSync(scriptFilePath, script);
   const testName = 'active-object-test-fail';
   (await test({ testName, moduleName, functionName: 'activate', testParams: { sessionId, url, scriptFilePath, input } })).assert((res) => res === false ? true : false);
   (await test({ testName, moduleName, functionName: 'call', testParams: { sessionId, url, scriptFilePath, input } })).assert((res) => res.message && res.stack);
--- tests/active-object-test-pass.js
+++ tests/active-object-test-pass.js
@@ -11,7 +11,7 @@ const sessionId = 'c62120de-30d4-40a1-8fef-0a6133efdee6';
   const { createSession } = await require('../lib/store');
   const { session } = await createSession({ sessionId });
   const { writeFileSync } = session;
-  writeFileSync(scriptFilePath, script);
+  await writeFileSync(scriptFilePath, script);
   const testName = 'active-object-test-pass';
   (await test({ testName, moduleName, functionName: 'activate', testParams: { sessionId, url, scriptFilePath, input } })).assert((res) => res === true ? true : false);
   (await test({ testName, moduleName, functionName: 'call', testParams: { sessionId, url, scriptFilePath, input } })).assert(({ func2 }) => func2);
