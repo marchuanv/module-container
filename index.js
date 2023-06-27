4ffcaeace6dd714d24a21575f7a64d9a02cf8cbc
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2023-06-27 03:59:55 +0200

    re-writing server

--- index.js
+++ index.js
@@ -2,100 +2,64 @@ const http = require("http");
 const logging = require('./lib/logging');
 const utils = require("utils");
 const path = require("path");
+const { createSession } = require('./lib/store');
+const activeObjEndpointExp = /^\/api\/v[0-9]+\/active-object\/[a-zA-Z]+$/g;
+const isGetExp = /active-object\/get/g;
+const isCreateExp = /active-object\/create/g;
+const isRemoveExp = /active-object\/remove/g;
+const isUpdateExp = /active-object\/update/g;
+const isDeleteExp = /active-object\/delete/g;
+const endpointsDirPath = path.join(__dirname, 'endpoints');
 
-require('./lib/store').createSession({ sessionId: 'f3ab396e-b549-4fbe-9eda-21570147f78a' }).then((session) => {
-    
-    const { writeFileSync, mkdirSync, existsSync, readFileSync, rmSync } = session;
-
-    const activeObjectsDir = path.join(__dirname, 'objects');
+createSession({ sessionId: 'f3ab396e-b549-4fbe-9eda-21570147f78a' }).then(({ session }) => {
     const server = http.createServer();
-    
     logging.setLevel({ level: 'info' });
-    
     server.on("request", (request, response) => {
-        let content = '';
+        let requestContent = '';
         let url = request.url.toLowerCase();
         request.on('data', (chunk) => {
-            content += chunk;
+            requestContent += chunk;
         });
         request.on('end', async () => {
-            content = content.toLowerCase();
-            console.log("request received");
-            const results = {
-                statusCode: 404,
-                statusMessage: 'Not Found',
-                message: 'No Active Objects Found'
-            };
-            const urlSplit = url.split('/').filter(x => x);
-            console.log('url segments: ', utils.getJSONString(urlSplit));
-            const aoName = urlSplit[0];
-            const functionName = urlSplit[1];
-            const activeObjectDirPath = path.join(activeObjectsDir, aoName);
-            const activeObjectScriptFilePath = path.join(activeObjectDirPath, `${aoName}-script.js`);
-            if (aoName) {
-                if (request.method === 'GET' && (await existsSync(activeObjectScriptFilePath)) ) {
-                    content = await readFileSync(activeObjectScriptFilePath);
-                    results.statusCode = 200;
-                    results.statusMessage = 'Success';
-                    results.message = 'Active Object Info';
-                    results.script = content;
-                } else if (request.method === 'PUT') {
-                    if (content) {
-                        if (!(await existsSync(activeObjectDirPath)) ) {
-                            await mkdirSync(activeObjectDirPath);
-                        }
-                        await writeFileSync(activeObjectScriptFilePath, content);
-                        const activeObject = (await require("./lib/active-object")({ url, scriptFilePath: activeObjectScriptFilePath }));
-                        if (activeObject.activate()) {  
-                            results.statusCode = 200;
-                            results.statusMessage = 'Success';
-                            results.message = 'Active Object Created/Updated';
-                        } else {
-                            results.statusCode = 500;
-                            results.statusMessage = 'Internal Server Error';
-                            results.message = 'Active Object Script Error';
-                        }
+            let statusCode = -1;
+            let statusMessage = '';
+            let handle = async () => { return { statusCode: 500, statusMessage: '500 Internal Server Error', responseContent: { message: 'Internal Server Error' } } };
+            try {
+                requestContent = utils.getJSONObject(requestContent);
+                activeObjEndpointExp.lastIndex = -1;
+                if (activeObjEndpointExp.test(url)) {
+                    isGetExp.lastIndex = -1;
+                    isCreateExp.lastIndex = -1;
+                    isRemoveExp.lastIndex = -1;
+                    isUpdateExp.lastIndex = -1;
+                    isDeleteExp.lastIndex = -1;
+                    if (isGetExp.test(url)) {
+                        ({ handle } = require(path.join(endpointsDirPath, `acitve-object-get.js`)));
+                    } else if (isCreateExp.test(url)) {
+                        ({ handle } = require(path.join(endpointsDirPath, `acitve-object-create.js`)));
+                    } else if (isRemoveExp.test(url)) {
+                        ({ handle } = require(path.join(endpointsDirPath, `acitve-object-remove.js`)));
+                    } else if (isUpdateExp.test(url)) {
+                        ({ handle } = require(path.join(endpointsDirPath, `acitve-object-update.js`)));
+                    } else if (isDeleteExp.test(url)) {
+                        ({ handle } = require(path.join(endpointsDirPath, `acitve-object-delete.js`)));
                     } else {
-                         results.statusCode = 400;
-                         results.statusMessage = 'Bad Request';
-                         results.message = 'No Active Object Script';
+                        handle = async () => { return { statusCode: 404, statusMessage: '404 Not Found', responseContent: { message: '404 Not Found' } } };
                     }
-                } else if (request.method === 'DELETE' && (await existsSync(activeObjectScriptFilePath)) ) {
-                    await rmSync(activeObjectScriptFilePath);
-                    results.statusCode = 200;
-                    results.statusMessage = 'Success';
-                    results.message = 'Active Object Deleted';
-                } else if (request.method === 'POST' && functionName && (await existsSync(activeObjectScriptFilePath)) ) {
-                    const activeObject = (await require("./lib/active-object")({ url, scriptFilePath: activeObjectScriptFilePath }));
-                    try {
-                        console.log(`executing the ${functionName} function.`);
-                        await activeObject.activate();
-                        const results = await activeObject.call({ input: content });
-                        if (results.message && results.stack) {
-                            throw new Error(message);
-                        }
-                        results.statusCode = 200;
-                        results.statusMessage = 'Success';
-                        results.message = 'Active Object Function Executed';
-                        results.results = results;
-                    } catch(error) {
-                        console.error(error);
-                        results.statusCode = 500;
-                        results.statusMessage = 'Internal Server Error';
-                        results.message = 'Active Object Script Error';
-                    }
+                } else {
+                    handle = async () => { return { statusCode: 404, statusMessage: '404 Not Found', responseContent: { message: '404 Not Found' } } };
                 }
+                ({ statusCode, statusMessage, responseContent } = await handle({ session, requestContent }));
+            } catch (err) {
+                console.log(err);
+                ({ statusCode, statusMessage, responseContent } = await handle({ session, requestContent }));
             }
-            const { statusCode, statusMessage } = results;
-            delete results.statusCode;
-            delete results.statusMessage;
-            const bodyStr = utils.getJSONString(results);
             response.writeHead(statusCode, statusMessage, {
-                'Content-Length': Buffer.byteLength(bodyStr),
+                'Content-Length': Buffer.byteLength(JSON.stringify(responseContent)),
                 'Content-Type': 'application/json'
             });
-            response.end(bodyStr);
+            response.end(JSON.stringify(responseContent));
         });
     });
-    server.listen(process.env.PORT || 8080);
+    server.listen(process.env.PORT || 80);
 });
