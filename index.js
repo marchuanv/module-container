d241eef42a97160679e27a3f26a8c13b091a0991
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2023-06-27 05:46:35 +0200

    re-writing server

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const logging = require('./lib/logging');
 const utils = require("utils");
 const path = require("path");
 const { createSession } = require('./lib/store');
+
 const activeObjEndpointExp = /^\/api\/v[0-9]+\/active-object\/[a-zA-Z]+\/[a-zA-Z0-9]+.json$/g;
-const isGetExp = /active-object\/get/g;
-const isCreateExp = /active-object\/create/g;
-const isRemoveExp = /active-object\/remove/g;
+const actionExpressions = [
+    /active-object\/get/g,
+    /active-object\/create/g,
+    /active-object\/remove/g
+];
+
 const endpointsDirPath = path.join(__dirname, 'lib', 'endpoints');
 
 createSession({ sessionId: 'f3ab396e-b549-4fbe-9eda-21570147f78a' }).then(({ session }) => {
@@ -27,16 +31,18 @@ createSession({ sessionId: 'f3ab396e-b549-4fbe-9eda-21570147f78a' }).then(({ ses
                 requestContent.url = url;
                 activeObjEndpointExp.lastIndex = -1;
                 if (activeObjEndpointExp.test(url)) {
-                    isGetExp.lastIndex = -1;
-                    isCreateExp.lastIndex = -1;
-                    isRemoveExp.lastIndex = -1;
-                    if (isGetExp.test(url)) {
-                        ({ handle } = require(path.join(endpointsDirPath, `active-object-get.js`)));
-                    } else if (isCreateExp.test(url)) {
-                        ({ handle } = require(path.join(endpointsDirPath, `active-object-create.js`)));
-                    } else if (isRemoveExp.test(url)) {
-                        ({ handle } = require(path.join(endpointsDirPath, `active-object-remove.js`)));
-                    } else {
+                    handle = null;
+                    for (const action of actionExpressions) {
+                        action.lastIndex = -1;
+                        if (action.test(url)) {
+                            action.lastIndex = -1;
+                            const results = action.exec(url);
+                            const fileName = results[0].replace(/\//g, '-');
+                            ({ handle } = require(path.join(endpointsDirPath, `${fileName}.js`)));
+                            break;
+                        }
+                    }
+                    if (!handle) {
                         handle = async () => { return { statusCode: 404, statusMessage: '404 Not Found', responseContent: { message: '404 Not Found' } } };
                     }
                 } else {
