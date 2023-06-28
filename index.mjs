28b2a00a29b37c04b7ce43b46feca736e082db7b
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2023-06-28 23:08:17 +0200

    refactor fixes to es6

--- index.mjs
+++ index.mjs
@@ -15,33 +15,35 @@ const logging = new Logging();
 const githubFile = new GithubFile({ utils, logging, github });
 const githubBranch = new GithubBranch({ logging, github });
 const store = new Store({ githubBranch, githubFile, utils, logging, path });
-const registry = new EndpointRegistry({ path, utils, store, logging, vm });
 
-logging.setLevel({ level: 'info' });
-const rootHandler = registry.findRootHandler(/v1/g);
-logging.log({ info: `using ${rootHandler.name} root handler.` });
-const server = http.createServer();
+store.login().then(() => {
+    const registry = new EndpointRegistry({ path, utils, store, logging, vm });
+    logging.setLevel({ level: 'info' });
+    const rootHandler = registry.findRootHandler(/v1/g);
+    logging.log({ info: `using ${rootHandler.name} root handler.` });
+    const server = http.createServer();
 
-server.on("request", (req, res) => {
-    const path = req.url.toLowerCase();
-    const headers = req.headers;
-    let content = '';
-    req.on('data', (chunk) => {
-        content += chunk;
-    });
-    req.on('end', async () => {
-        let { statusCode, statusMessage, responseContent, contentType } = {};
-        try {
-            ({ statusCode, statusMessage, responseContent, contentType } = await rootHandler.handle({ path, content, headers }));
-        } catch (error) {
-            logging.log({ info: 'root handler should handle all errors, this should not happen.' });
-            logging.log({ error });
-        }
-        res.writeHead(statusCode, statusMessage, {
-            'Content-Length': Buffer.byteLength(responseContent),
-            'Content-Type': contentType
+    server.on("request", (req, res) => {
+        const path = req.url.toLowerCase();
+        const headers = req.headers;
+        let content = '';
+        req.on('data', (chunk) => {
+            content += chunk;
         });
-        res.end(responseContent);
+        req.on('end', async () => {
+            let { statusCode, statusMessage, responseContent, contentType } = {};
+            try {
+                ({ statusCode, statusMessage, responseContent, contentType } = await rootHandler.handle({ path, content, headers }));
+            } catch (error) {
+                logging.log({ info: 'root handler should handle all errors, this should not happen.' });
+                logging.log({ error });
+            }
+            res.writeHead(statusCode, statusMessage, {
+                'Content-Length': Buffer.byteLength(responseContent),
+                'Content-Type': contentType
+            });
+            res.end(responseContent);
+        });
     });
+    server.listen(process.env.PORT || 80);
 });
-server.listen(process.env.PORT || 80);
