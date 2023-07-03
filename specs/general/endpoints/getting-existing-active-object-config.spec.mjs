ef46bda4921a154fd00fe366f98b23b4e14b94f0
marchuanv <marchuanv@gmail.com>
2023-07-03 22:21:10 +0200

    more specs

---
+++ specs/general/endpoints/getting-existing-active-object-config.spec.mjs
@@ -0,0 +1,33 @@
+import {
+    Container
+} from '../../../lib/registry.mjs';
+fdescribe('when-getting-existing-active-object-config-from-store', () => {
+    let statusMessage;
+    let responseContent;
+    beforeAll( async () => {
+        let { $logging, $store, $getConfigEndpoint, $createConfigEndpoint } = new Container();
+        $logging.setToInfo();
+        await $store.login();
+        {
+            const { statusMessage } = await $createConfigEndpoint.handle({ content: JSON.stringify({ 
+                className: 'HelloWorld',
+                language: 'JavaScript',
+                dependencyInjection: false
+            })});
+            expect(statusMessage).toBe('200 Success');
+        }
+        ({ statusMessage, responseContent } = await $getConfigEndpoint.handle());
+        expect(statusMessage).toBe('200 Success');
+        responseContent = JSON.parse(responseContent);
+    });
+    it('should return content', () => {
+        expect(responseContent).toBeDefined();
+        expect(responseContent.className).toBe('HelloWorld');
+        expect(responseContent.language).toBe('JavaScript');
+        expect(responseContent.dependencyInjection).toBeFalse();
+    });
+    afterAll( async () => {
+        let { $store } = new Container();
+        await $store.logout();
+    });
+});
