58e5aaf3fc38b8c53dcd22435320cefbdd8bd58c
marchuanv <marchuanv@gmail.com>
2023-07-03 21:24:42 +0200

    fixes

--- index.mjs
+++ index.mjs
@@ -1,10 +1,9 @@
 import { Container } from './lib/registry.mjs'
 class Server {
     start() {
-        const { $endpointRegistry, $logging, $http } = new Container();
+        const { $endpointRegistry, $logging, $http, $store } = new Container();
         const rootHandler = $endpointRegistry.findHandler(/v1/g);
-        $logging.setLevel({ level: 'info' });
-        $logging.log({ info: `using ${rootHandler.name} root handler.` });
+        $logging.setToInfo();
         const server = $http.createServer();
         server.on("request", (req, res) => {
             const path = req.url.toLowerCase();
@@ -14,13 +13,14 @@ class Server {
                 content += chunk;
             });
             req.on('end', async () => {
+                await $store.login();
                 let { statusCode, statusMessage, responseContent, contentType } = {};
                 try {
                     ({ statusCode, statusMessage, responseContent, contentType } = await rootHandler.handle({ path, content, headers }));
                 } catch (error) {
-                    $logging.log({ info: 'root handler should handle all errors, this should not happen.' });
                     $logging.log({ error });
                 }
+                await $store.logout();
                 res.writeHead(statusCode, statusMessage, {
                     'Content-Length': Buffer.byteLength(responseContent),
                     'Content-Type': contentType
