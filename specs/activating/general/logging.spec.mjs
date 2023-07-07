54c0703a190d2815289dfa648cccae4b3b2bfbeb
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2023-07-07 17:05:23 +0200

    fixes

--- specs/activating/general/logging.spec.mjs
+++ specs/activating/general/logging.spec.mjs
@@ -4,16 +4,10 @@ import {
 describe('when-activating-logging', () => {
     let logging;
     beforeAll(() => {
-        logging = new Logging();
+        logging = new Logging({ logLevel: 'error' });
     });
     it('should create an instance', () => {
         expect(logging).toBeDefined();
-    });
-    it('should have a setToInfo member', () => {
-        expect(logging.setToInfo).toBeDefined();
-    });
-    it('should have a setToError member', () => {
-        expect(logging.setToError).toBeDefined();
     });
     it('should have a log member', () => {
         expect(logging.log).toBeDefined();
