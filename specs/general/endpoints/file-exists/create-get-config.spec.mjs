d84e099af13701bcd8b3492485d0d5eb3b5cdf84
marchuanv <marchuanv@gmail.com>
2023-07-07 02:01:23 +0200

    rule to make all container class members async

--- specs/general/endpoints/file-exists/create-get-config.spec.mjs
+++ specs/general/endpoints/file-exists/create-get-config.spec.mjs
@@ -2,7 +2,7 @@ import {
     allEndpoints
 } from '../../../../lib/endpoints/registry.mjs';
 jasmine.DEFAULT_TIMEOUT_INTERVAL = 120000;
-fdescribe('when getting config from the store given that the file exists', () => {
+describe('when getting config from the store given that the file exists', () => {
     let { message, content } = {};
     beforeAll(async () => {
         const args = {
