b6e6adf2ad12783c4da647616590fc36b34752b7
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2023-07-11 22:41:23 +0200

    refactor

--- specs/fakes/github-fake.mjs
+++ specs/fakes/github-fake.mjs
@@ -17,58 +17,70 @@ export class GithubFake extends Container {
         return new Promise(async (resolve, reject) => {
             if (this.locked) {
                 setTimeout(async () => {
-                    resolve((await request({ route, parameters })));
+                    try {
+                        const output = await this.request({ route, parameters });
+                        resolve(output);
+                    } catch (err) {
+                        reject(err);
+                    }
                 }, 1000);
             } else {
-                this.locked = true;
-                this.heads.delete('heads');
-                this.heads.set('heads', { data: [{ object: { sha: 12345678 } }] });
-                let isBranch = /\/branch/g.test(route);
-                let isBranchRef = /git\/refs\/heads/g.test(route);
-                if (!isBranchRef) {
-                    isBranchRef = /\/git\/refs/g.test(route);
-                }
-                let fileMatches = /[a-zA-Z0-9\-]+\.((json)|(js))/g.exec(route);
-                if (route.indexOf('GET /repos') > -1) {
-                    if (isBranch) {
-                        let branch = this.branches.get('testing');
-                        if (branch) {
-                            return resolve(true);
-                        } else {
-                            this.locked = false;
-                            return reject(new Error('branch does not exist'));
+                try {
+                    this.locked = true;
+                    this.heads.delete('heads');
+                    this.heads.set('heads', { data: [{ object: { sha: 12345678 } }] });
+                    let isBranch = /\/branch/g.test(route);
+                    let isBranchRef = /git\/refs\/heads/g.test(route);
+                    if (!isBranchRef) {
+                        isBranchRef = /\/git\/refs/g.test(route);
+                    }
+                    let fileMatches = /[a-zA-Z0-9\-]+\.((json)|(js))/g.exec(route);
+                    if (route.indexOf('GET /repos') > -1) {
+                        if (isBranch) {
+                            let branch = this.branches.get('testing');
+                            if (branch) {
+                                return resolve(true);
+                            } else {
+                                return reject(new Error('branch does not exist'));
+                            }
+                        }
+                        if (isBranchRef) {
+                            return resolve(this.heads.get('heads'));
+                        }
+                        if (fileMatches.length > 0) {
+                            const id = fileMatches[0];
+                            let data = this.data.get(id);
+                            if (data) {
+                                data = utils.base64ToString(data);
+                            }
+                            return resolve({ data });
                         }
                     }
-                    if (isBranchRef) {
-                        return resolve(this.heads.get('heads'));
-                    }
-                    if (fileMatches.length > 0) {
-                        const id = fileMatches[0];
-                        let data = this.data.get(id);
-                        if (data) {
-                            data = utils.base64ToString(data);
+                    if (route.indexOf('PUT /repos') > -1) {
+                        if (fileMatches.length > 0) {
+                            const id = fileMatches[0];
+                            this.data.delete(id);
+                            this.data.set(id, parameters.content);
+                            return resolve();
                         }
-                        return resolve({ data });
                     }
-                }
-                if (route.indexOf('PUT /repos') > -1) {
-                    if (fileMatches.length > 0) {
-                        const id = fileMatches[0];
-                        this.data.delete(id);
-                        this.data.set(id, parameters.content);
-                        return resolve();
+                    if (route.indexOf('POST /repos') > -1) {
+                        if (isBranchRef) {
+                            const id = 'testing';
+                            this.branches.delete(id);
+                            this.branches.set(id, 'testing');
+                            return resolve();
+                        }
                     }
+
+                    return reject(new Error('Not Implemented'));
                 }
-                if (route.indexOf('POST /repos') > -1) {
-                    if (isBranchRef) {
-                        const id = 'testing';
-                        this.branches.delete(id);
-                        this.branches.set(id, 'testing');
-                        return resolve();
-                    }
+                catch (err) {
+                    reject(err);
                 }
-                this.locked = false;
-                return reject(new Error('Not Implemented'));
+                finally {
+                    this.locked = false;
+                }
             }
         });
     }
