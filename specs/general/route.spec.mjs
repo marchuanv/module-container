2364e02fb19efa09a8c891c5f1b94a89f0633cd3
marchuanv <marchuanv@gmail.com>
2023-07-18 22:50:28 +0200

    refactor

--- specs/general/route.spec.mjs
+++ specs/general/route.spec.mjs
@@ -1,7 +1,14 @@
 import { Route } from '../../lib/registry.mjs';
-fdescribe('when-handling-routing-given-get-config-endpoint', () => {
+describe('when-handling-routing-given-get-config-endpoint', () => {
     let response;
     beforeAll(async () => {
+        const route_delete = new Route({
+            path: '/api/v1/config/delete',
+            token: process.env.GIT
+        });
+        response = await route_delete.handle();
+        expect(response).toBeDefined();
+        expect(response.statusCode).toBe(200);
         const route_create = new Route({
             path: '/api/v1/config/create',
             content: JSON.stringify({
@@ -11,11 +18,43 @@ fdescribe('when-handling-routing-given-get-config-endpoint', () => {
             }),
             token: process.env.GIT
         });
-        await route_create.handle();
+        response = await route_create.handle();
+        expect(response).toBeDefined();
+        expect(response.statusCode).toBe(200);
         const route_get = new Route({ path: '/api/v1/config/get', token: process.env.GIT });
         response = await route_get.handle();
     });
     it('should instruct get-config-endpoint to handle the request and return a success response', () => {
+        expect(response).toBeDefined();
+        expect(response.statusCode).toBe(200);
+    });
+});
+describe('when-handling-routing-given-get-class-endpoint', () => {
+    let response;
+    beforeAll(async () => {
+        const route_delete = new Route({
+            path: '/api/v1/class/delete',
+            token: process.env.GIT
+        });
+        response = await route_delete.handle();
+        expect(response).toBeDefined();
+        expect(response.statusCode).toBe(200);
+        const route_create = new Route({
+            path: '/api/v1/class/create',
+            content: JSON.stringify({
+                className: 'HelloWorld',
+                language: 'JavaScript',
+                dependencyInjection: false
+            }),
+            token: process.env.GIT
+        });
+        response = await route_create.handle();
+        expect(response).toBeDefined();
+        expect(response.statusCode).toBe(200);
+        const route_get = new Route({ path: '/api/v1/class/get', token: process.env.GIT });
+        response = await route_get.handle();
+    });
+    it('should instruct get-class-endpoint to handle the request and return a success response', () => {
         expect(response).toBeDefined();
         expect(response.statusCode).toBe(200);
     });
