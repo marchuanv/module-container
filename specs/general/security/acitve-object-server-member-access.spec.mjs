1b4efb4b302e472a22c2f9dce3125e3a9b32ccc4
marchuanv <marchuanv@gmail.com>
2023-07-22 00:12:10 +0200

    refactor

--- specs/general/security/acitve-object-server-member-access.spec.mjs
+++ specs/general/security/acitve-object-server-member-access.spec.mjs
@@ -16,7 +16,7 @@ describe('when-accessing-server-member-from-different-context', () => {
         expect(error.message).toBe('Unable to access member: server, no public member events was on the event stack');
     });
 });
-fdescribe('when-accessing-github-fake-member-from-different-context', () => {
+describe('when-accessing-github-fake-member-from-different-context', () => {
     let error;
     beforeAll(async () => {
         const github = new GithubFake({ auth: process.env.GIT });
