47a155236865bfb8e7c4d5174ed13dde930c6614
marchuanv <marchuanv@gmail.com>
2023-07-22 01:07:42 +0200

    refactor

---
+++ specs/general/server.spec.mjs
@@ -0,0 +1,13 @@
+import { ActiveObjectServer, Route } from '../../lib/registry.mjs';
+describe('when-creating-an-active-object-server-given-a-request-for-geting-config', () => {
+    let response;
+    beforeAll(async () => {
+        const server = new ActiveObjectServer();
+        await server.start();
+        response = await fetch('http://localhost/api/v1/config/get');
+    });
+    it('should handle request and setup a route', async () => {
+        expect(response).toBeDefined();
+        expect(response.status).toBe(404);
+    });
+});
