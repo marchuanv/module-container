2dff334a9ecc28cdc88b9bc0fd7bdf09eb4df79b
marchuanv <marchuanv@gmail.com>
2023-07-23 04:22:23 +0200

    better logging

---
+++ specs/general/logging.spec.mjs
@@ -0,0 +1,23 @@
+import { Logging } from '../../lib/logging.mjs';
+describe('when logging messages given context', () => {
+    let logging;
+    beforeAll((done) => {
+        logging = new Logging({ contextId: 'CONTEXT A' });
+        logging.log('Hello World 1');
+        logging.log('Hello World 2');
+        logging.log('Hello World 3');
+        logging.log('Hello World 4');
+        setTimeout(() => {
+            logging = new Logging({ contextId: 'CONTEXT B' });
+            logging.log('Hello World 5');
+            logging.log('Hello World 6');
+            logging.log('Hello World 7');
+            logging.log('Hello World 8');
+            setTimeout(() => {
+                done();
+            } ,1000);
+        } ,1000);
+    });
+    it('should display messages', async () => {
+    });
+});
