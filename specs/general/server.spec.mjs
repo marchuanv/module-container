f8de686e41e10ca4aa6ba06cfcb18fc4d4933da9
marchuanv <marchuanv@gmail.com>
2023-07-26 01:00:11 +0200

    fixing specs passing usernames

--- specs/general/server.spec.mjs
+++ specs/general/server.spec.mjs
@@ -1,10 +1,14 @@
 import { ActiveObjectServer, Route } from '../../lib/registry.mjs';
-describe('when-creating-an-active-object-server-given-a-request-for-geting-config', () => {
+fdescribe('when-creating-an-active-object-server-given-a-request-for-geting-config', () => {
     let response;
     beforeAll(async () => {
         const server = new ActiveObjectServer();
         await server.start();
-        response = await fetch(`http://localhost:${process.env.PORT || 80}/api/v1/config/get`);
+        response = await fetch(`http://localhost:${process.env.PORT || 80}/api/v1/config/get`,{ 
+            method: 'GET',
+            headers: {
+               username: 'JoE' 
+            }});
     });
     it('should handle request and setup a route', async () => {
         expect(response).toBeDefined();
