1e63af2845a7a0036f9bf7af4305071b992d222f
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2023-07-29 22:39:49 +0200

    test

--- specs/config-template.spec.mjs
+++ specs/config-template.spec.mjs
@@ -1,14 +1,15 @@
 import vm from 'node:vm'
-class SomeClass {}
+class SomeClass { }
 class ContainerConfigTemplate {
     constructor({ config }) {
         this.config = config;
-        this.template = { members: { 
-                any_1: { 
+        this.template = {
+            members: {
+                any_1: {
                     class: Object,
                     args: Object
                 },
-                any_2: { 
+                any_2: {
                     callback: Function,
                     args: Object
                 },
@@ -23,42 +24,43 @@ class ContainerConfigTemplate {
                     mockClass: Object,
                     args: Object
                 }
-            } 
+            }
         };
         this.messages = [];
     }
     validate(configItem = this.config, templateItem = this.template) {
         const configItemType = typeof configItem;
         const templateItemType = typeof templateItem;
-        if (configItemType === templateItemType) {
-            if (templateItemType === 'object') {
-                for(const configKey of Object.keys(configItem)) {
-                    const configValue = configItem[configKey];
-                    const unmatchedTemplateKeys = [];
-                    for(const templateKey of Object.keys(templateItem)) {
-                        const templateValue = templateItem[templateKey];
-                        this.messages.push(`config[${key}] argument passed to ${this.contextName} class constructor is null or undefined.`);
-                    }
-                    if (configValue === undefined || configValue === null) {
-                        this.messages.push(`config[${key}] argument passed to ${this.contextName} class constructor is null or undefined.`);
+        if (configItemType === templateItemType && templateItemType === 'object') {
+            for (const configKey of Object.keys(configItem)) {
+                const configValue = configItem[configKey];
+                for (const templateKey of Object.keys(templateItem)) {
+                    const templateValue = templateItem[templateKey];
+                    if (this.validate(configValue, templateValue)) {
+
+                    } else {
+                        this.messages.push(`config[${templateKey}] argument passed to ${this.contextName} constructor is null or undefined.`);
                     }
                 }
-            } else {
-                for(const configKey of Object.keys(configItem)) {
-                    const configValue = configItem[configKey];
-                    const unmatchedTemplateKeys = [];
-                    for(const templateKey of Object.keys(templateItem)) {
-                        const templateValue = templateItem[templateKey];
-                        if (Object.keys(configValue) && Object.keys(templateValue)) {
-                            
-                        }
+            }
+            if (this.messages.length === 0) {
+                this.messages.push(`config[${key}] argument passed to ${this.contextName} class constructor is null or undefined.`);
+            }
+        } else {
+            for (const configKey of Object.keys(configItem)) {
+                const configValue = configItem[configKey];
+                const unmatchedTemplateKeys = [];
+                for (const templateKey of Object.keys(templateItem)) {
+                    const templateValue = templateItem[templateKey];
+                    if (Object.keys(configValue) && Object.keys(templateValue)) {
 
-                       
-                        this.messages.push(`config[${key}] argument passed to ${this.contextName} class constructor is null or undefined.`);
                     }
-                    if (configValue === undefined || configValue === null) {
-                        this.messages.push(`config[${key}] argument passed to ${this.contextName} class constructor is null or undefined.`);
-                    }
+
+
+                    this.messages.push(`config[${key}] argument passed to ${this.contextName} class constructor is null or undefined.`);
+                }
+                if (configValue === undefined || configValue === null) {
+                    this.messages.push(`config[${key}] argument passed to ${this.contextName} class constructor is null or undefined.`);
                 }
             }
         }
@@ -72,7 +74,7 @@ fdescribe('when providing container config the config template matcher', () => {
             members: {
                 path: { value: "some path" },
                 content: { value: 'class HelloWord { sayHello() { console.log("Hello"); } }' },
-                someClass: { class: { SomeClass }, args: { } },
+                someClass: { class: { SomeClass }, args: {} },
                 filePath: { value: "c:/test/test/test" },
                 vm: { value: vm }
             },
@@ -80,7 +82,7 @@ fdescribe('when providing container config the config template matcher', () => {
                 singleton: false,
                 errorHalt: true
             },
-            mocks: { }
+            mocks: {}
         };
         const containerConfigTempalte = new ContainerConfigTemplate({ config });
         containerConfigTempalte.validate();
