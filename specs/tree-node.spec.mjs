d7aa83981ecff492d3e6e0a270be8de48ae9b965
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2023-08-01 14:24:31 +0200

    test

--- specs/tree-node.spec.mjs
+++ specs/tree-node.spec.mjs
@@ -1,44 +1,65 @@
-import { TreeNode, TreeNodeTemplate } from '../lib/container/tree-node.mjs';
+import { ContainerConfigNode, ObjectNodeTemplate } from '../lib/container/object-tree.mjs';
+import { Github, GithubMock } from '../lib/registry.mjs';
 fdescribe('when creating a', () => {
-    it('should', (done) => {
-        const containerConfig = new TreeNodeTemplate('container', [
-            new TreeNodeTemplate('members', [
-                new TreeNodeTemplate('any', [
-                    new TreeNodeTemplate('class', {}),
-                    new TreeNodeTemplate('args', {})
+    it('should', () => {
+        const configTemplate = new ObjectNodeTemplate('container', [
+            new ObjectNodeTemplate('members', [
+                new ObjectNodeTemplate('any', [
+                    new ObjectNodeTemplate('class', {}),
+                    new ObjectNodeTemplate('args', {})
                 ]),
-                new TreeNodeTemplate('any', [
-                    new TreeNodeTemplate('value', {})
+                new ObjectNodeTemplate('any', [
+                    new ObjectNodeTemplate('value', {})
                 ]),
-                new TreeNodeTemplate('any', [
-                    new TreeNodeTemplate('callback', {}),
-                    new TreeNodeTemplate('args', {})
+                new ObjectNodeTemplate('any', [
+                    new ObjectNodeTemplate('callback', {}),
+                    new ObjectNodeTemplate('args', {})
                 ])
             ]),
-            new TreeNodeTemplate('behavior', [
-                new TreeNodeTemplate('singleton', false),
-                new TreeNodeTemplate('errorHalt', true)
+            new ObjectNodeTemplate('behavior', [
+                new ObjectNodeTemplate('singleton', false),
+                new ObjectNodeTemplate('errorHalt', true)
             ]),
-            new TreeNodeTemplate('mocks', [
-                new TreeNodeTemplate('any', [
-                    new TreeNodeTemplate('class', {}),
-                    new TreeNodeTemplate('classMock', {}),
-                    new TreeNodeTemplate('args', {})
-                ])
+            new ObjectNodeTemplate('mocks', [
+                new ObjectNodeTemplate('class', {}),
+                new ObjectNodeTemplate('classMock', {}),
+                new ObjectNodeTemplate('args', {})
             ])
         ]);
-
-        let child = containerConfig.child;
-        while (child) {
-            console.log(child.toString());
-            child = child.child;
-        }
-
-        // const node = new TreeNode(config, containerConfig);
-        // if (node.matchTemplate()) {
-        //     console.log('matches template');
-        // } else {
-        //     console.log('does not match template');
-        // }
+        const containerConfig = {
+            container: {
+                members: {
+                    github: {
+                        class: { Github },
+                        args: { auth: 'storeAuthToken' }
+                    },
+                    branchName: {
+                        value: 'branchName'
+                    },
+                    fileName: {
+                        value: 'fileName'
+                    },
+                    jsFileNameExp: {
+                        value: /^.js$/g
+                    },
+                    jsonFileNameExp: {
+                        value: /^.json$/g
+                    },
+                },
+                mocks: {
+                    githubMock: {
+                        class: { Github },
+                        mockClass: { GithubMock },
+                        args: { auth: 'storeAuthToken' }
+                    }
+                },
+                behaviour: {
+                    singleton: false,
+                    errorHalt: false
+                }
+            }
+        };
+        const containerConfigNode = new ContainerConfigNode(containerConfig, configTemplate);
+        console.log(containerConfigNode.toString());
     });
 });
