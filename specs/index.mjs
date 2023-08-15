cf056c25d009c4555abb8c3c9f3f19f67405362a
marchuanv <marchuanv@gmail.com>
2023-08-15 13:23:44 +0200

    refactor

--- specs/index.mjs
+++ specs/index.mjs
@@ -3,7 +3,6 @@ import * as url from 'url';
 process.env.LOG = JSON.stringify({ enabled: false, onExit: true });
 const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
 const jasmine = new Jasmine({ projectBaseDir: __dirname });
+jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000;
 jasmine.addMatchingSpecFiles(['**/*.spec.mjs']);
-jasmine.DEFAULT_TIMEOUT_INTERVAL = 30000;
 jasmine.execute();
-// console.clear();
