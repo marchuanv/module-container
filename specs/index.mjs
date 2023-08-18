41f2e78a1b493bf347166102c6f9628ecc2aa805
marchuanv <marchuanv@gmail.com>
2023-08-19 00:44:18 +0200

    environment switch

--- specs/index.mjs
+++
@@ -1,8 +0,0 @@
-import Jasmine from 'jasmine';
-import * as url from 'url';
-process.env.LOG = JSON.stringify({ enabled: false, onExit: true });
-const __dirname = url.fileURLToPath(new URL('.', import.meta.url));
-const jasmine = new Jasmine({ projectBaseDir: __dirname });
-jasmine.jasmine.DEFAULT_TIMEOUT_INTERVAL = 20000;
-jasmine.addMatchingSpecFiles(['**/*.spec.mjs']);
-jasmine.execute();
