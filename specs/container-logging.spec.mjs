2fe6983943477e8f49f82bcd1d22eae9273145d0
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2023-08-23 18:08:55 +0200

    logging

--- specs/container-logging.spec.mjs
+++
@@ -1,7 +0,0 @@
-import { TestClass } from './class.mjs';
-describe('when logging messages given a container extends logging', () => {
-    it('should inherently write messages', () => {
-        const instance = new TestClass();
-        instance.log('test message');
-    });
-});
---
+++ specs/container.logging.spec.mjs
@@ -0,0 +1,26 @@
+import { TestClass, TestClassDependency } from './class.mjs';
+fdescribe('when creating an instance of a TestClass given a TestClassDependency', () => {
+    it('should create call all members and log accordingly', async () => {
+        let error;
+        let testClass;
+        let testClassDependency;
+        let propertyValue;
+        try {
+            testClass = new TestClass();
+            testClassDependency = await testClass.getTestClassDependency();
+            propertyValue = await testClassDependency.getProperty();
+            propertyValue = 'overwrite';
+            await testClassDependency.setProperty(propertyValue);
+            propertyValue = await testClassDependency.getProperty();
+        } catch (err) {
+            error = err;
+        }
+        expect(error).not.toBeDefined();
+        expect(testClass).toBeDefined();
+        expect(propertyValue).toBe('overwrite');
+        expect(propertyValue).toBeDefined();
+        expect(testClassDependency).toBeDefined();
+        expect(testClass).toBeInstanceOf(TestClass);
+        expect(testClassDependency).toBeInstanceOf(TestClassDependency);
+    });
+});
