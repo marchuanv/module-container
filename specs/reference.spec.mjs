e3ab1aa62693ea01e4dffa6c3963ed60f0c67429
Marchuan Van Der Merwe <marchuanv@sahomeloans.com>
2023-08-24 17:52:11 +0200

    adjustments

--- specs/reference.spec.mjs
+++ specs/reference.spec.mjs
@@ -4,8 +4,8 @@ fdescribe('when creating a reference', () => {
         let error;
         let references;
         try {
-            const refA = Reference.create({ name: 'referenceA', object: { Id: 'referenceAId' } });
-            Reference.create({ name: 'referenceB', object: { Id: 'referenceBId' }, reference: refA });
+            const refA = Reference.create({ object: { name: 'referenceA', Id: 'referenceAId' } });
+            Reference.create({ object: { name: 'referenceB', Id: 'referenceBId' }, reference: refA });
             references = Reference.references({ Id: 'referenceBId' });
         } catch (err) {
             error = err;
